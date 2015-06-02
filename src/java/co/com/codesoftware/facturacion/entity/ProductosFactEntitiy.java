ce988b2be586f432d1a4ce571d4de8e84869e461
jnsierra <jnsierrac@gmail.com>
2015-06-01 22:33:37 -0500

    111
    1111

---
+++ src/java/co/com/codesoftware/facturacion/entity/ProductosFactEntitiy.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.codesoftware.facturacion.entity;
+
+/**
+ *
+ * @author nicolas
+ */
+public class ProductosFactEntitiy {
+
+    private String dska_dska;
+    private String dska_cant;
+    private String descuento;
+
+    public String getDska_dska() {
+        return dska_dska;
+    }
+
+    public void setDska_dska(String dska_dska) {
+        this.dska_dska = dska_dska;
+    }
+
+    public String getDska_cant() {
+        return dska_cant;
+    }
+
+    public void setDska_cant(String dska_cant) {
+        this.dska_cant = dska_cant;
+    }
+
+    public String getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(String descuento) {
+        this.descuento = descuento;
+    }
+
+}
